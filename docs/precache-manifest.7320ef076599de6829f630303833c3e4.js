9fce50ca1ac1b5c6e88fa1581dded30182c7364b
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-09 11:24:47 +1000

    first build

---
+++ docs/precache-manifest.7320ef076599de6829f630303833c3e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889215d135e6d7851aca41b3980a0f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5113e244b2cc8e4f265",
+    "url": "/static/css/main.2fdbeddf.chunk.css"
+  },
+  {
+    "revision": "e941398d646b6731ff57",
+    "url": "/static/js/2.7fe2817d.chunk.js"
+  },
+  {
+    "revision": "d5113e244b2cc8e4f265",
+    "url": "/static/js/main.53d2c7d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
